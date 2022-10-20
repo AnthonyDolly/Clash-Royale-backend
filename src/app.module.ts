eb28fba09501acf8226d4484029f9f067a76f62b
anthonydolly <anthonyluiggydollyo@gmail.com>
2022-10-19 21:31:38 -0500

    Configuracion global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { UsersModule } from './users/users.module';
+import { MembersModule } from './members/members.module';
+import { CommonModule } from './common/common.module';
+import { EnvConfiguration } from './config/app.config';
+import { JoiValidationSchema } from './config/joi.validation';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
+    }),
+    UsersModule,
+    MembersModule,
+    CommonModule,
+    MongooseModule.forRoot(process.env.MONGODB),
+    AuthModule,
+  ],
 })
 export class AppModule {}
