1b5f36d8b40c845fb59e32c3d372663431cd1fdd
Anthony Dolly <anthonyluiggydollyo@gmail.com>
2022-12-13 11:19:07 -0500

    Agregando proteccion a la ruta

--- src/members-winners/members-winners.controller.ts
+++ src/members-winners/members-winners.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { MembersWinnersService } from './members-winners.service';
 import { CreateMembersWinnerDto } from './dto/create-members-winner.dto';
+import { Auth } from './../auth/decorators';
+import { ValidRoles } from './../auth/interfaces';
 
 @Controller('members-winners')
 export class MembersWinnersController {
   constructor(private readonly membersWinnersService: MembersWinnersService) {}
 
+  @Auth(ValidRoles.LIDER)
   @Post()
   create(@Body() createMembersWinnerDto: CreateMembersWinnerDto) {
     return this.membersWinnersService.create(createMembersWinnerDto);
